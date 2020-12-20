1276b98a9391ca0a297f4aaf3f541fc3615370d1
futhimhlas <futhimhl2@gmail.com>
2020-12-20 16:39:41 +0000

    comments added

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,12 +1,12 @@
 function fetchGitHubInformation(event){
 
-    var username = $("#gh-username").val();
-    if (!username) {
+    var username = $("#gh-username").val(); //here we are using jquery to select the # id and then specifing we are targetting the value
+    if (!username) {                    // if the var username is empty ie no value in the form > this happens
         $("#gh-user-data").html(`<h2>Please enter a Github username</h2>`);
         return;
     }
 
-    $("#gh-user-data").html(
+    $("#gh-user-data").html(  // this is the else part of the statement meaning if text has been inputted
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`
